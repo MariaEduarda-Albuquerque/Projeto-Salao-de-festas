1d84f7e095d0c496ccc6a9d67a1db35f999c43f8
Maria Eduarda Albuquerque <dudinhazilda17@gmail.com>
2025-12-17 21:14:00 -0300

    Add files via upload

---
+++ Cardapio/Coquetel Básico/js/coqutel basico.js
@@ -0,0 +1,17 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "./images.coqutel basico/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "./images.coqutel basico/close_white_36dp.svg";
+    }
+}
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 4000)
